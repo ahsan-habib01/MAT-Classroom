0867c31260a9259a6dc9d80b610699e7a2779786
ahsan.habib01 <ahshapon00@gmail.com>
2026-04-07 11:51:49 +0600

    again

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,8 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import GoogleProvider from 'next-auth/providers/google'; 
+import GoogleProvider from 'next-auth/providers/google';
 
-export const authOptions = {
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
